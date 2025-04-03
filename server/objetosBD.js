655b7e08bf4260ef601c62ad88cf23e8f34f35c4
Music-Vibes-Rep <myvibesmymusic@gmail.com>
2025-04-03 16:00:13 +0000

    feat:
    ivan: Creo un js en un fichero objetosBD, en el que hay un json con objetos de la bd para acceder mas rapido en un futuro a todos los atributos de la base de datos e importar

---
+++ server/objetosBD.js
@@ -0,0 +1,28 @@
+//vmaos a colocar objetos de las bases de datos 
+
+//usuario
+
+const usuario = {
+    id_usuario: null,
+    Nombre: '',
+    Apellido: '',
+    email: '',
+    password: '',
+    foto_perfil: '',
+    descripcion: '',
+    fecha_creacion: null,
+    es_musico: 0,
+    id_instrumento: null,
+    id_provincia: null
+  };
+
+//publicaciones
+
+const publicacion = {
+    id_publicacion: null,
+    fecha_publicacion: null,
+    contenido: '',
+    foto: '',
+    id_usuario: null
+  };
+  
